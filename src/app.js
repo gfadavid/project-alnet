e708a64a39d3ca153a7b36025f1516eb34a47167
Ángel González <gfadavid240@gmail.com>
2020-11-16 02:31:00 -0400

    Entorno de desarrollo listo.

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// Importano versión SASS de Bootstrap 4.5.3
+import 'bootstrap';
+// Importando complementes necesario del framework
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/alert';
+// Archivo CSS generado con Webpack
+import './styles/styles.scss'; // Override a las variables por defecto de Bootstrap
+
+
+import './static/img/foto.jpg';
---
+++ src/webpack/webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+
+    entry: './src/app.js', // Archivo que maneja la aplicación
+
+    output: {
+        path: path.resolve(__dirname, '../../dist'), // La ruta que se va a generar
+        filename: 'js/bundle.js' // Nombre de archivo que va a generar
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'static/img',
+                            useRelativePath: true
+                        }
+                    },
+                    // { 
+                    //     loader: 'image-webpack-loader',
+                    //     options: {
+                    //       mozjpeg: {
+                    //         progressive: true,
+                    //       },
+                    //       // optipng.enabled: false will disable optipng
+                    //       optipng: {
+                    //         enabled: false,
+                    //       },
+                    //       pngquant: {
+                    //         quality: [0.65, 0.90],
+                    //         speed: 4
+                    //       },
+                    //       gifsicle: {
+                    //         interlaced: false,
+                    //       },
+                    //       // the webp option will enable WEBP
+                    //       webp: {
+                    //         quality: 75
+                    //       }
+                    //     }
+                    // },
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            minify: false
+        }),
+
+        new MiniCSSExtractPlugin({
+            filename: 'css/[name].css'
+        })
+    ]
+
+};
